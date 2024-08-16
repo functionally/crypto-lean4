b3370f23685fa5cf26e8e6d042e669af40ec3dbb
Brian W Bush <code@functionally.io>
2024-08-16 10:59:13 -0600

    Refactored field.

--- src/Crypto/Field/Fp.lean
+++ src/Crypto/Field/Fp.lean
@@ -1,98 +1,89 @@
 namespace Crypto.Field
 
 
-inductive Fp (p : Int) where
-| mk : Int → Fp p
-| infinity : Fp p
-deriving Repr
-
-namespace Fp
-
-  def wellFormed : Fp p → Prop
-  | mk x => x % p = x
-  | infinity => True
-
-end Fp
-
-instance : BEq (Fp p) where
-  beq
-  | Fp.mk x, Fp.mk y => x % p == y % p
-  | Fp.infinity, Fp.infinity => true
-  | Fp.infinity, _ => false
-  | _, Fp.infinity => false
-
-instance : OfNat (Fp p) n where
-  ofNat := Fp.mk n
-
-instance : Add (Fp p) where
-  add x y := Fp.mk $ (x.value + y.value) % p
-
-instance : Sub (Fp p) where
-  sub x y := ⟨ (x.value - y.value) % p ⟩
-
-instance : Mul (Fp p) where
-  mul x y := ⟨ (x.value * y.value) % p ⟩
-
-private partial def gcdExtended (a : Int) (b : Int) : Int × Int × Int :=
-  -- FIXME: Prove termination.
-  if a == 0
+private def gcdExtended (a : Nat) (b : Nat) : Nat × Nat × Nat :=
+  if a = 0
     then ⟨ b, 0, 1 ⟩
     else let ⟨ gcd, x', y' ⟩ := gcdExtended (b % a) a
          let x := y' - (b / a) * x'
          let y := x'
          ⟨ gcd, x, y ⟩
+termination_by a
+decreasing_by
+  simp_wf
+  apply Nat.mod_lt
+  apply Nat.zero_lt_of_ne_zero
+  assumption
 
-def inverse (x : Fp p) : Fp p :=
-  let ⟨ gcd , y , _ ⟩ := gcdExtended x.value p
-  if gcd == 1
-    then ⟨ y ⟩
-    else none
-
-instance : Div (Fp p) where
-  div x y := x * inverse y
-
-instance : Neg (Fp p) where
-  neg x := ⟨ (- x.value) % p ⟩
-
-private partial def modExp (m : Int) (x : Int) : Nat → Int
-  -- FIXME: Prove termination.
-  | 0 => 1
-  | n => let y := modExp m (x * x % m) (n / 2)
+private def modExp (m : Nat) (x : Nat) (n : Nat) : Nat :=
+  if n = 0
+    then 1
+    else let y := modExp m (x * x % m) (n / 2)
          if n % 2 == 1
            then x * y % m
            else y
 
+
+structure Fp (p : Nat) where
+  private mkUnsafe ::
+  val : Nat
+deriving Repr
+
+namespace Fp
+
+  def mk (x : Nat) : Fp p :=
+    Fp.mkUnsafe $ x % p
+
+  def canonical (x : Fp p) : Prop :=
+    x.val < p
+
+  def nonzero (x : Fp p) : Prop :=
+    ¬ x.val % p = 0
+
+  def invertible (x : Fp p) : Prop :=
+    let ⟨ gcd , _ , _ ⟩ := gcdExtended x.val p
+    gcd = 1
+
+  /-
+  theorem nonzero_invertible (x : Fp p) : nonzero x → invertible x := by
+    sorry
+
+  theorem invertible_nonzero (x : Fp p) : invertible x → nonzero x := by
+    sorry
+  -/
+
+  def inverse (x : Fp p) : Option (Fp p) :=
+    let ⟨ gcd , y , _ ⟩ := gcdExtended x.val p
+    if gcd = 1
+      then some ⟨ y ⟩
+      else none
+
+end Fp
+
+instance : BEq (Fp p) where
+  beq x y := x.val % p == y.val % p
+
+instance : OfNat (Fp p) n where
+  ofNat := Fp.mk n
+
+instance : Add (Fp p) where
+  add x y := ⟨ (x.val + y.val) % p ⟩
+
+instance : Neg (Fp p) where
+  neg x := ⟨ (p - x.val) % p ⟩
+
+instance : Sub (Fp p) where
+  sub x y := ⟨ (x.val - y.val) % p ⟩
+
+instance : Mul (Fp p) where
+  mul x y := ⟨ (x.val * y.val) % p ⟩
+
+/-
+instance : Div (Fp p) where
+  div x y := x * Fp.inverse y
+-/
+
 instance : Pow (Fp p) Nat where
-  pow x n := ⟨ modExp p x.value n ⟩
-
-/-
-private theorem power_zero (x : Fp p) : Pow.pow x 0 = ⟨ 1 ⟩ := by
-  simp [Pow.pow, modExp]
-
-private theorem power_one (x : Fp p) : Fp.wellFormed x → x = Pow.pow x 1 := by
-  intro h
-  simp [Pow.pow, modExp]
-  rw [h]
-
--- FIXME: Prove the general case.
--/
-
-/-x
-inductive Montgomery (p : Int) where
-| mk (r : Fp p) (rinv : Fp p) : Option.isSome (inverse r) → Montgomery p
-
-#check Montgomery.mk ⟨ 1 ⟩ ⟨ 2 ⟩
--/
-
-/-
-structure Montgomery (p : Int) where
-  r : Fp p
---valid  := Option.isSome (inverse r) = true
-  valid  := Option.isSome (inverse r) = true
-  rinv : Fp p :=
-    let h := valid = true
-    Option.get (inverse r) $ by
-      sorry
--/
+  pow x n := ⟨ modExp p x.val n ⟩
 
 end Crypto.Field
